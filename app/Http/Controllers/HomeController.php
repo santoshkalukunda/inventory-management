ccdafe02b4c7a249e18365c1b4a4fd967462ff65
santoshkalukunda <santoshkalukunda@gmail.com>
2021-03-09 16:34:20 +0545

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bill;
+use App\Models\Customer;
+use App\Models\Dealer;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $totalIncome = 0;
+        $dueBill = 0;
+        $totalPurchase = 0;
+        $duePurchase = 0;
+
+        $customers = Customer::get();
+        $dealers = Dealer::get();
+        $bills = Bill::get();
+        foreach ($bills as $bill){
+            $totalIncome = $totalIncome + $bill->net_total;
+            $dueBill = $dueBill + $bill->due;
+        }
+        $purchases = Purchase::get();
+        foreach ($purchases as $purchase){
+            $totalPurchase = $totalPurchase + $purchase->total;
+            $duePurchase = $duePurchase + $purchase->due;
+        }
+        return view('home', compact('customers','dealers','totalIncome','dueBill','totalPurchase','duePurchase'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,46 @@
 @endsection
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="ibox bg-success color-white widget-stat">
+                <div class="ibox-body">
+                    <h2 class="m-b-5 font-strong">{{$customers->count()}}</h2>
+                    <div class="m-b-5">Customers</div><i class="fa fa-users widget-stat-icon"></i>
+                    <div><i class="fa fa-level-up m-r-5"></i><small></small></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="ibox bg-info color-white widget-stat">
+                <div class="ibox-body">
+                    <h2 class="m-b-5 font-strong">{{$dealers->count()}}</h2>
+                    <div class="m-b-5">Dealers</div><i class="fa fa-store widget-stat-icon"></i>
+                    <div><i class="fa fa-level-up m-r-5"></i><small></small></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="ibox bg-warning color-white widget-stat">
+                <div class="ibox-body">
+                    <h2 class="m-b-5 font-strong">{{$totalIncome}}</h2>
+                    <div class="m-b-5">TOTAL INCOME</div><i class="fa fa-money-check-alt widget-stat-icon"></i>
+                    <div><i class="fa fa-level-up m-r-5"></i><small>Due : {{$dueBill}} </small></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="ibox bg-danger color-white widget-stat">
+                <div class="ibox-body">
+                    <h2 class="m-b-5 font-strong">{{$totalPurchase}}</h2>
+                    <div class="m-b-5">Expenditure</div><i class="fa fa-hand-holding-usd widget-stat-icon"></i>
+                    <div><i class="fa fa-level-down m-r-5"></i><small>Due : {{$duePurchase}}</small></div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row justify-content-center">
+        
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
