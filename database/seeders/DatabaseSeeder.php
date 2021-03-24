f5e0ccb5171fd6f794226e5d84de6d4f3af5103c
santoshkalukunda <santoshkalukunda@gmail.com>
2021-03-24 17:42:25 +0545

    seeder customer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CustomerSeeder::class,
+            // CustomerSeeder::class,
             UserSeeder::class,
             CompanySeeder::class,
         ]);
